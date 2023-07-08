3846551eebb226f5cdb9a14ca1a556952992bdf2
anandamrit1 <anandamrit53@gmail.com>
2023-07-08 21:36:49 +0530

    added constants for more question types

--- src/utils/QuestionUtils.ts
+++ src/utils/QuestionUtils.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_SHORT_ANSWER_QUESTION } from "./constants"
+import { DEFAULT_LONG_ANSWER_QUESTION, DEFAULT_MULTIPLE_OPTION_ANSWER_QUESTION, DEFAULT_SHORT_ANSWER_QUESTION, DEFAULT_SINGLE_OPTION_ANSWER_QUESTION } from "./constants"
 import { v4 as uuidv4 } from 'uuid';
 
 export const CreateNewQuestion = (questionType: string) => {
@@ -11,6 +11,14 @@ export const GetDefaultQuestion = (questionType: string) => {
     switch (questionType) {
         case 'shortAnswer':
             return DEFAULT_SHORT_ANSWER_QUESTION
+        case 'longAnswer':
+            return DEFAULT_LONG_ANSWER_QUESTION
+        case 'singleOption':
+            return DEFAULT_SINGLE_OPTION_ANSWER_QUESTION
+        case 'multipleOption':
+            return DEFAULT_MULTIPLE_OPTION_ANSWER_QUESTION
+        case 'email':
+            return DEFAULT_LONG_ANSWER_QUESTION
         default:
             return DEFAULT_SHORT_ANSWER_QUESTION
     }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,47 @@ export const DEFAULT_SHORT_ANSWER_QUESTION = {
     },
     "formFieldId": "shortAnswer"
 }
+
+export const DEFAULT_LONG_ANSWER_QUESTION = {
+    "title": "What is your name?",
+    "description": "Please enter your full name",
+    "type": "longAnswer",
+    "required": false,
+    "properties": {
+        "placeholder": "Your Answer"
+    },
+    "formFieldId": "11"
+}
+
+export const DEFAULT_SINGLE_OPTION_ANSWER_QUESTION = {
+    "title": "What is your name?",
+    "description": "Please enter your full name",
+    "type": "singleOption",
+    "required": false,
+    "properties": {
+        "options": ["Option 1", "Option 2", "Option 3"]
+    },
+    "formFieldId": "singleOption"
+}
+
+export const DEFAULT_MULTIPLE_OPTION_ANSWER_QUESTION = {
+    "title": "What is your name?",
+    "description": "Please enter your full name",
+    "type": "multipleOption",
+    "required": false,
+    "properties": {
+        "options": ["Option 1", "Option 2", "Option 3"]
+    },
+    "formFieldId": "1234"
+}
+
+export const DEFAULT_EMAIL_ANSWER_QUESTION = {
+    "title": "What is your email?",
+    "description": "Please enter your email",
+    "type": "email",
+    "required": false,
+    "properties": {
+        "placeholder": "Your Email"
+    },
+    "formFieldId": "12345"
+}
